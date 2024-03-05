037b0c2d7dc30d0d4de348ada698d54cea0f671e
Dheeraj Kumar Kondapalli <dheerajkumar200005@gmail.com>
2024-03-05 11:57:07 -0600

    added comments

--- src/main/java/com/ecomm/security/Controllers/AuthController.java
+++ src/main/java/com/ecomm/security/Controllers/AuthController.java
@@ -59,6 +59,7 @@ public class AuthController {
 
     @PostMapping("/refreshToken")
     public RefreshTokenResponseDto refreshToken(@RequestHeader("Authorization") String bearerToken) {
+        //to return the refresh token
         return tokenService.findByToken(bearerToken.substring(7,bearerToken.length()))
                 .map(tokenService::verifyExpiration)
                 .map(UniqueToken::getUser)
--- src/main/java/com/ecomm/security/DTO/RefreshTokenRequestDto.java
+++ src/main/java/com/ecomm/security/DTO/RefreshTokenRequestDto.java
@@ -4,6 +4,7 @@ import lombok.Data;
 
 @Data
 public class RefreshTokenRequestDto {
+    //delete this dto!!!!
     public String jwtToken;
     public String uniqueToken;
 }
